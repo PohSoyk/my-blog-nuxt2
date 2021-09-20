a7b867e51f74b937efdfb6383ecb9dc36f0c8d1b
PoSo <hokuro.jump@gmail.com>
2021-09-20 10:44:35 +0900

    [update]目次を開閉できるようにする

--- components/Toc.vue
+++ components/Toc.vue
@@ -1,13 +1,31 @@
 <template>
   <div v-if="visible" class="wrapper">
-    <h4 class="title">目次</h4>
-    <ul class="lists">
-      <li v-for="item in toc" :key="item.id" :class="`list ${item.name}`">
-        <n-link v-scroll-to="`#${item.id}`" to>
-          {{ item.text }}
-        </n-link>
-      </li>
-    </ul>
+    <h4 class="title" @click="isShow()">
+      <transition name="rotate" mode="out-in">
+        <img
+          v-if="show"
+          key="rotate1"
+          src="/images/icon_caret_down_solid.svg"
+          alt="目次"
+        />
+        <img
+          v-else
+          key="rotate2"
+          src="/images/icon_caret_right_solid.svg"
+          alt="目次"
+        />
+      </transition>
+      目次
+    </h4>
+    <transition name="open">
+      <ul v-if="show" class="lists">
+        <li v-for="item in toc" :key="item.id" :class="`list ${item.name}`">
+          <n-link v-scroll-to="`#${item.id}`" to>
+            {{ item.text }}
+          </n-link>
+        </li>
+      </ul>
+    </transition>
   </div>
 </template>
 
@@ -25,6 +43,16 @@ export default {
       default: false,
     },
   },
+  data() {
+    return {
+      show: true,
+    }
+  },
+  methods: {
+    isShow() {
+      this.show = !this.show
+    },
+  },
 }
 </script>
 
@@ -40,6 +68,17 @@ export default {
   font-weight: bold;
   margin: 0 0 10px;
   border-radius: 5px;
+  text-decoration: underline;
+  display: inline-block;
+
+  &:hover {
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  & img {
+    vertical-align: -5%;
+  }
 }
 
 .list {
@@ -64,5 +103,29 @@ export default {
   &.h3 {
     margin-left: 20px;
   }
+}
+@keyframes open {
+  0% {
+    opacity: 0;
+    transform: translateY(-5px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+}
+.open-enter-active {
+  animation: open 0.2s;
+}
+.open-leave-active {
+  animation: open 0.1s linear reverse;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(180deg);
+  }
+}
+.rotate-enter-active {
+  animation: rotate 0.45s linear;
 }
 </style>
